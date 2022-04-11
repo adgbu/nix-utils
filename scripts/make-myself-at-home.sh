483dc5fe8990d9aec32120fdc99476a2ba5722d0
Greger Burman <greger.burman@adopticum.se>
2022-04-11 23:49:27 +0200

    Append bashrc, don't replace it on RPi.

--- scripts/make-myself-at-home.sh
+++ scripts/make-myself-at-home.sh
@@ -4,6 +4,7 @@
 # It will setup some things that I like to have on all systems.
 # Make yourself at home!
 
+BASE=$HOME/rpi-utils
 
 if [ $# -eq 2 ]; then
 	# Configure git with personal info.
@@ -15,11 +16,14 @@ if [ $# -eq 2 ]; then
 fi
 
 echo "Hooking you up with some sweet command aliases..."
-ln -sf "$HOME/rpi-utils/bash/bash_aliases" "$HOME/.bash_aliases"
+ln -sf "$BASE/bash/bash_aliases" "$HOME/.bash_aliases"
 
 echo "Hooking you up with a custom bash prompt..."
-ln -sf "$HOME/rpi-utils/bash/bash_prompt" "$HOME/.bash_prompt"
+ln -sf "$BASE/bash/bash_prompt" "$HOME/.bash_prompt"
 printf "\n# Custom bash prompt\n. ~/.bash_prompt" >> ~/.bashrc
+
+echo "Appending .bashrc to get things rolling..."
+cat "$BASE/bash/bashrc" >> "$HOME/.bashrc"
 
 # ~/.local/bin will be in PATH automatically iff it exists.
 echo "Creating ~/.local/bin to put local executables in..."
